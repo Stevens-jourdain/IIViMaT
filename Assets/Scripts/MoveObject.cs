b457f91d4e5f78ea98ffb60b4c9772598a895f93
Stevens-jourdain <stevens.jourdain@etudiant.univ-lille1.fr>
2018-02-14 13:30:46 +0100

    Bug menu

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -35,8 +35,7 @@ public class MoveObject : MonoBehaviour
     {
         if (TimeToMove == 2)
         {
-            Vector3 position = new Vector3(), angleRot = new Vector3();
-            Quaternion rotation = new Quaternion();            
+            Vector3 position = new Vector3(), angleRot = new Vector3();      
             
             // scale
             if (Input.GetKeyUp(KeyCode.KeypadPlus))            
@@ -102,12 +101,37 @@ public class MoveObject : MonoBehaviour
             }
 
             // HTC Vive Controller
-           /* if (main.rightDevice != null && main.leftDevice != null)
+            if (main.rightDevice != null && main.leftDevice != null)
             {
-                // Définition echelle et position
-                scale = main.rightDevice.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x + 1 * 5;
-                position = main.right.transform.position;
-                rotation = main.right.transform.rotation;
+                // Définition echelle et position en hauteur
+                float rightX = main.rightDevice.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+                float rightY = main.rightDevice.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).y;
+                scale += rightX > 0.5f ? (0.5f * (rightX+1)) : (rightX < -0.5f ? (-0.5f * (-rightX + 1)) : 0);
+                position.y += rightY > 0.5f ? (0.01f * (rightY + 1)) : (rightY < -0.5f ? (-0.01f * (-rightY + 1)): 0);
+
+                // Position par rapport au sol
+                float leftX = main.leftDevice.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+                float leftY = main.leftDevice.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).y;
+                position.x = leftX > 0.5f ? (0.01f * (leftX + 1)): (leftX < -0.5f ? (-0.01f * (-leftX + 1)) : 0);
+                position.z = leftY > 0.5f ? (0.01f * (leftY + 1)): (leftY < -0.5f ? (-0.01f * (-leftY + 1)) : 0);
+
+                // Rotation de l'objet
+                Vector3 anglePad = main.right.transform.rotation.eulerAngles;
+
+                if (anglePad.x > 0.5f)
+                    angleRot.x += (0.1f * (anglePad.x + 1));
+                if (anglePad.x < -0.5f)
+                    angleRot.x -= (0.1f * (-anglePad.x + 1));
+
+                if (anglePad.y > 0.5f)
+                    angleRot.y += (0.1f * (anglePad.y + 1));
+                if (anglePad.y > 0.5f)
+                    angleRot.y += (0.1f * (-anglePad.y + 1));
+
+                if (anglePad.z > 0.5f)
+                    angleRot.z += (0.1f * (anglePad.z + 1));
+                if (anglePad.z > 0.5f)
+                    angleRot.z += (0.1f * (-anglePad.z + 1));
 
                 // Si on valide
                 if (triggerManager.OnTriggerUp(main.rightDevice) || triggerManager.OnTriggerUp(main.leftDevice))
@@ -121,23 +145,14 @@ public class MoveObject : MonoBehaviour
                     TimeToMove = 0;
                     main.actionEnCours = false;                    
                 }
-            }*/
+            }
 
             // Application pour chaque objet
             foreach (GameObject obj in listOfObject)
             {
                 obj.transform.localScale = new Vector3(scale, scale, scale);
-
-                /*if (main.rightDevice != null && main.leftDevice != null)
-                {
-                    obj.transform.position = position;
-                    obj.transform.rotation = rotation;
-                }
-                else*/
-                //{
-                    obj.transform.Translate(position);
-                    obj.transform.Rotate(angleRot * 360);
-                //}
+                obj.transform.Translate(position);                
+                obj.transform.Rotate(angleRot * 360);                
             }
         }
 
@@ -147,8 +162,8 @@ public class MoveObject : MonoBehaviour
             if (Input.GetKeyUp(KeyCode.KeypadEnter))
                 TimeToMove = 2;
 
-            //if (main.rightDevice != null && main.leftDevice != null && (triggerManager.OnTriggerUp(main.rightDevice) || triggerManager.OnTriggerUp(main.leftDevice)))
-               // TimeToMove = 2;
+            if (main.rightDevice != null && main.leftDevice != null && (triggerManager.OnTriggerUp(main.rightDevice) || triggerManager.OnTriggerUp(main.leftDevice)))
+                TimeToMove = 2;
         }
     }
 }
